3d520aaf2a83029e861389c07aaed64f2dc01160
Alban Cavey <albancavey@gmail.com>
2022-03-01 12:50:18 +0100

    Feature engineering on nb of visits per year

---
+++ care_for_health/feature_engineering.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+def med_g_statistics():
+    stats = {
+    '18-24':[6, 21, 17, 22, 8, 11, 4, 9, 2],
+    '25-34': [6, 24, 18, 16, 8, 9, 6, 9, 4],
+    '35-49': [3, 24, 17, 16, 9, 12, 9, 8, 2],
+    '50-64': [1, 13, 12, 11, 27, 10, 10, 12, 4],
+    '65-': [1, 5, 15, 10, 29, 12, 13, 12, 3],
+    }
+    y_visits_med_g = {i: sum(np.array(stats[i])*0.01*np.array([0, 1, 2, 3, 4, 5, 6, 9.5, 14]).T) for i in stats}
+    return y_visits_med_g
+
+def yearly_med_g_visits(df, y_visits_med_g):
+    df['med_g_visites_annuelles'] = 0
+    df.loc[:,'med_g_visites_annuelles'] = round(df.loc[:,'P18_POP0014']*3+\
+                                    df.loc[:,'P18_POP1529']*y_visits_med_g['18-24']+\
+                                    df.loc[:,'P18_POP3044']*y_visits_med_g['25-34']+\
+                                    df.loc[:,'P18_POP4559']*y_visits_med_g['35-49']+\
+                                    df.loc[:,'P18_POP6074']*y_visits_med_g['50-64']+\
+                                    df.loc[:,'P18_POP7589']*y_visits_med_g['65-']+\
+                                    df.loc[:,'P18_POP90P']*y_visits_med_g['65-'], 2)
+    return df
